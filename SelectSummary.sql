0ec16814e0bbb65092ad580589b567881a3aa216
kw cho <ckw7407@naver.com>
2022-10-18 17:13:16 +0900

    second selectSummary

--- SelectSummary.sql
+++ SelectSummary.sql
@@ -120,6 +120,92 @@ select empno,ename,sal,deptno
 from emp
 where deptno = 10;
 
+select 'Welcome',upper('Welcome')
+from dual;
+
+select lower(ename),upper(ename)
+from emp;
+
+select *
+from emp
+where lower(ename) = 'scott';
+
+
+select ename,length(ename)
+from emp;
+
+select 'Welcome to Oracle',substr('Welcome to Oracle',2,3),substr('Welcome to Oracle',10)
+from dual;
+
+select 'Welcome to Oracle',substr('Welcome to Oracle',-3,3),substr('Welcome to Oracle',-17)
+from dual;
+
+select instr('Welcome to Oracle','o')
+from dual;
+
+select instr('Welcome to Oracle','o',6)
+from dual;
+
+select instr('Welcome to Oracle','e',3,2)
+from dual;
+
+select 'Welcome to Oracle',replace('Welcome to Oracle','to','of')
+from dual;
+
+select 'oracle',lpad('oracle',10,'#'),rpad('oracle',10,'*'),lpad('oracle',10)
+from dual;
+
+select  rpad('990103-',14,'*')
+from dual;
+
+select concat(empno,ename), empno || '' || ename
+from emp;
+
+select 
+	round(1234.5678),
+	round(1234.5678,0),
+	round(1234.5678,1),
+	round(1234.5678,2),
+    round(1234.5678,-1)
+from dual;
+
+select 
+	trunc(1234.5678),
+	trunc(1234.5678,0),
+	trunc(1234.5678,1),
+	trunc(1234.5678,2),
+	trunc(1234.5678,-1)
+from dual;
+
+
+select 
+	ceil(3.14),  --   자신보다 큰 가장 가까운 정수       4
+	floor(3.14), --  자신보다 작은 가장 가까운 정수    3
+	ceil(-3.14),  --   -3
+	floor(-3.14) --   -4
+from dual;
+
+select mod(5,2),mod(10,4)
+from dual;
+
+select *
+from emp
+where mod(empno,2) = 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
